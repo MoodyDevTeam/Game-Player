4c8ff84bf14683db6ddd81425f67ca30edae0a3c
Yashas Shah <79524299+YashasTheDev@users.noreply.github.com>
2021-04-20 14:28:48 -0400

    Create colors.h

---
+++ colors.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+#define GREEN cout << "\033[0;32m" << endl;
+#define BLUE cout << "\033[0;34m" << endl;
+#define RED cout << "\033[0;31m" << endl;
+#define CYAN cout << "\033[0;36m" << endl;
+#define ORANGE cout << "\033[0;33m" << endl;
+#define PURPLE cout << "\033[0;35m" << endl;
+#define WHITE cout << "\033[0;37m" << endl;
